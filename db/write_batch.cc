1a581360621efff9a4528838701e016dae1509fa
Li_diang <787695954@qq.com>
2023-04-12 10:52:38 +0800

    modified:   db/write_batch.cc

--- db/write_batch.cc
+++ db/write_batch.cc
@@ -112,8 +112,6 @@ void WriteBatch::Put(const Slice& key, const Slice& value) {
   PutLengthPrefixedSlice(&rep_, key);
   // 加入value.size()
   PutLengthPrefixedSlice(&rep_, value);
-  std::cout << std::__cxx11::to_string(kTypeValue) << " " << std::__cxx11::to_string(key) << " "
-            << std::__cxx11::to_string(value) << std::endl;
 }
 
 void WriteBatch::Delete(const Slice& key) {
