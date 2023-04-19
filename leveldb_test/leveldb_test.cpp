05ba2c3775bd78228af430df2bc711c5cdb1265f
Li_diang <787695954@qq.com>
2023-04-19 11:21:33 +0800

    本地已经添加了Remix结构，且已完成了全局视图的创建，现在为了测试1000000的键值对写入时，全局视图的创建情况，因为本地跑太慢了

--- leveldb_test/leveldb_test.cpp
+++ leveldb_test/leveldb_test.cpp
@@ -1,20 +1,20 @@
-﻿/*
- * @Author: Li_diang 787695954@qq.com
- * @Date: 2023-04-04 22:59:17
- * @LastEditors: Li_diang 787695954@qq.com
- * @LastEditTime: 2023-04-12 10:23:54
- * @FilePath: \MYLevelDB\leveldb_test\leveldb_test.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 
 #include "leveldb/db.h"
 #include "leveldb/write_batch.h"
-#define N 1000
+
+#include "../db/Remix.h"
+#define N 900000
 using namespace std;
 using namespace leveldb;
 int main() {
+  /*
+    Segment A;
+    Remix B;
+    cout << B.empty() << endl;
+  */
+
   // 打开数据库
   leveldb::DB* mydb;
   // 初始化leveldb的memtable内存大小、文件操作接口、block大小、打开文件数限制等
@@ -40,15 +40,17 @@ int main() {
   }*/
 
   // 批量写入
-    /*for (int i = 0; i < N; i++) {
-      std::string key = std::to_string(i);
-      std::string value = std::to_string(i);
-      mydb->Put(leveldb::WriteOptions(), key, value);
-      assert(mystatus.ok());
-    }*/
+
+  for (int i = 0; i < N; i++) {
+    std::string key = std::to_string(i);
+    std::string value = std::to_string(i);
+    mydb->Put(leveldb::WriteOptions(), key, value);
+    assert(mystatus.ok());
+  }
+  Remix* my_sorted_view = new Remix(mydb);
 
   // 按照顺序批量写入
-  leveldb::WriteBatch batch;
+  /*leveldb::WriteBatch batch;
   leveldb::Status s;
   for (int i = 0; i < N; i++) {
     std::string key = std::to_string(i);
@@ -56,9 +58,9 @@ int main() {
     // mydb->Put(leveldb::WriteOptions(), key, value);
     batch.Put(key, value);
   }
+
   s = mydb->Write(leveldb::WriteOptions(), &batch);
-  assert(s.ok());
-  
+  assert(s.ok());*/
 
   // 读取数据
   // std::string key_ = "gonev";
@@ -67,22 +69,33 @@ int main() {
   // std::cout << key_ << ": " << val_ << std::endl;
 
   // 遍历levelDB中的所有数据
-   leveldb::Iterator* it = mydb->NewIterator(leveldb::ReadOptions());
-   for (it->SeekToFirst(); it->Valid(); it->Next()) {
-     std::cout << it->key().ToString() << ": " << it->value().ToString()
-               << std::endl;
-   }
+/*  leveldb::Iterator* it = mydb->NewIterator(leveldb::ReadOptions());
+  for (it->SeekToFirst(); it->Valid(); it->Next()) {
+    //std::cout << it->key().ToString() << ": " << it->value().ToString()
+    //<< std::endl;
+    //cout << endl;
+  }*/
 
   // 范围查询
-  /*leveldb::Iterator* it = mydb->NewIterator(leveldb::ReadOptions());
-  std::string start = "1";
-  std::string limit = "123";
+  /*
+  leveldb::Iterator* it = mydb->NewIterator(leveldb::ReadOptions());
+  std::string start = "97800";
+  std::string limit = "97810";
+  for (it->Seek(start); it->Valid() && it->key().ToString() < limit;
+       it->Next()) {
+    std::cout << it->key().ToString() << ": "
+        << it->value().ToString()
+              << std::endl;
+  }
+
+  start = "10000";
+  limit = "10010";
   for (it->Seek(start); it->Valid() && it->key().ToString() < limit;
        it->Next()) {
     std::cout << it->key().ToString() << ": " << it->value().ToString()
               << std::endl;
-  }*/
-
+  }
+  */
   // 逆序遍历
   /*leveldb::Iterator* it = mydb->NewIterator(leveldb::ReadOptions());
   for (it->SeekToLast(); it->Valid(); it->Prev()) {
@@ -105,3 +118,4 @@ int main() {
               << std::endl;
   }*/
 }
+
